f1bb0011cf11796bce0f3cc3f4473713f4cd9789
Lauren Delaney <ren.delaney@gmail.com>
2016-11-25 21:01:57 +0000

    base

--- testSharp/Form1.Designer.cs
+++ testSharp/Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.BurlyWood;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(113, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(334, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get the Top Artists on LastFm";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(631, 126);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "REST Simple Demo on LastFm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- testSharp/Form1.cs
+++ testSharp/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web;
+using RestSharp;
+using RestSharp.Deserializers;
 
 namespace testSharp
 {
@@ -15,6 +18,66 @@ namespace testSharp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?");
+            var request = new RestRequest();
+
+            // Set up and build parameter list for this service
+
+            //string Myuser = "test";
+            //string myMethod = "user.getTopArtists";
+            //request.AddParameter("method", myMethod);
+            //request.AddParameter("user", Myuser);
+            //string myKey = "581cca30b41a4cc0d5b3eb59d502b651";
+            //request.AddParameter("api_key", myKey);
+            //request.AddParameter("limit", 10);
+            //request.AddParameter("format", "json");
+            //string key = "71897F1E822664717BC6FEFAE509510A";
+            //request.AddParameter("api key", key);
+            request.AddParameter("appid", "440");
+            request.AddParameter("count", "3");
+            request.AddParameter("maxlength", "300");
+            request.AddParameter("format", "json");
+
+            // Service call
+
+            var response = client.Execute(request);
+
+
+            JsonDeserializer deserializer = new JsonDeserializer();
+
+            // have a look at the structure of the json
+
+            MessageBox.Show(response.Content);
+
+            var temp = deserializer.Deserialize<RootObject>(response); 
+
+        }
+
+        public class appnews{
+            public int appid { get; set; }
+            public List<newsitems> newsItems { get; set; }
+        }
+
+        public class newsitems
+        {
+            public int gid { get; set; }
+            public string title { get; set; }
+            public string url { get; set; }
+            public Boolean is_external_url { get; set; }
+            public string author { get; set; }
+            public string  contents { get; set; }
+            public string feedlabel { get; set; }
+            public int date { get; set; }
+            public string feedname { get; set; }
+
+    }
+        public class RootObject
+        {
+            public appnews appNews { get; set; }
         }
     }
 }
