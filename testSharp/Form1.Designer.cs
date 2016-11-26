fbf1b4ca6f6f4629c929dd96c3c91c06136017c8
Lauren Delaney <ren.delaney@gmail.com>
2016-11-26 01:08:35 +0000

    Added styling

--- testSharp/Form1.Designer.cs
+++ testSharp/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.txtGameId = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -39,9 +40,9 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.BurlyWood;
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(178, 15);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(176, 35);
@@ -60,6 +61,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(19, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 13);
@@ -77,6 +79,7 @@
             // labelPageNum
             // 
             this.labelPageNum.AutoSize = true;
+            this.labelPageNum.ForeColor = System.Drawing.Color.White;
             this.labelPageNum.Location = new System.Drawing.Point(175, 339);
             this.labelPageNum.Name = "labelPageNum";
             this.labelPageNum.Size = new System.Drawing.Size(24, 13);
@@ -107,6 +110,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Navy;
             this.ClientSize = new System.Drawing.Size(384, 374);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnPrev);
@@ -115,8 +119,10 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtGameId);
             this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Latest Game News";
+            this.Text = "Steam Game News";
             this.ResumeLayout(false);
             this.PerformLayout();
 
