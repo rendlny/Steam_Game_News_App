0270fc818a0636c8d760f4d8484429bb4a29a22e
Lauren Delaney <ren.delaney@gmail.com>
2016-11-28 16:21:45 +0000

    Removed recently played request because of problems with it

--- testSharp/Form1.Designer.cs
+++ testSharp/Form1.Designer.cs
@@ -29,31 +29,23 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
             this.txtGameId = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.txtNewsBox = new System.Windows.Forms.TextBox();
             this.labelPageNum = new System.Windows.Forms.Label();
             this.btnPrev = new System.Windows.Forms.Button();
             this.btnNext = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(178, 15);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(176, 35);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Get Latest News\r\n";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtGameId
             // 
-            this.txtGameId.Location = new System.Drawing.Point(22, 25);
+            this.txtGameId.Location = new System.Drawing.Point(59, 59);
             this.txtGameId.Name = "txtGameId";
             this.txtGameId.Size = new System.Drawing.Size(137, 20);
             this.txtGameId.TabIndex = 1;
@@ -61,16 +53,18 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(19, 9);
+            this.label1.Location = new System.Drawing.Point(56, 41);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(88, 15);
             this.label1.TabIndex = 2;
             this.label1.Text = "Enter Game ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtNewsBox
             // 
-            this.txtNewsBox.Location = new System.Drawing.Point(22, 68);
+            this.txtNewsBox.Location = new System.Drawing.Point(263, 30);
             this.txtNewsBox.Multiline = true;
             this.txtNewsBox.Name = "txtNewsBox";
             this.txtNewsBox.Size = new System.Drawing.Size(334, 255);
@@ -79,46 +73,127 @@
             // labelPageNum
             // 
             this.labelPageNum.AutoSize = true;
+            this.labelPageNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.labelPageNum.ForeColor = System.Drawing.Color.White;
-            this.labelPageNum.Location = new System.Drawing.Point(175, 339);
+            this.labelPageNum.Location = new System.Drawing.Point(416, 310);
             this.labelPageNum.Name = "labelPageNum";
-            this.labelPageNum.Size = new System.Drawing.Size(24, 13);
+            this.labelPageNum.Size = new System.Drawing.Size(24, 15);
             this.labelPageNum.TabIndex = 4;
             this.labelPageNum.Text = "0/0";
             // 
             // btnPrev
             // 
-            this.btnPrev.Location = new System.Drawing.Point(72, 334);
+            this.btnPrev.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnPrev.ForeColor = System.Drawing.Color.White;
+            this.btnPrev.Location = new System.Drawing.Point(322, 299);
             this.btnPrev.Name = "btnPrev";
-            this.btnPrev.Size = new System.Drawing.Size(58, 23);
+            this.btnPrev.Size = new System.Drawing.Size(71, 33);
             this.btnPrev.TabIndex = 5;
             this.btnPrev.Text = "Previous";
-            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.UseVisualStyleBackColor = false;
             this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
             // 
             // btnNext
             // 
-            this.btnNext.Location = new System.Drawing.Point(243, 334);
+            this.btnNext.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btnNext.ForeColor = System.Drawing.Color.White;
+            this.btnNext.Location = new System.Drawing.Point(460, 299);
             this.btnNext.Name = "btnNext";
-            this.btnNext.Size = new System.Drawing.Size(58, 23);
+            this.btnNext.Size = new System.Drawing.Size(71, 33);
             this.btnNext.TabIndex = 6;
             this.btnNext.Text = "Next";
-            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.UseVisualStyleBackColor = false;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(397, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "News";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(56, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 15);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Example Game ID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(59, 221);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(137, 20);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.Text = "252490";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(59, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 37);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Get Latest News";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(59, 288);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(137, 20);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "https://steamdb.info/apps/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(56, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 15);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Get Game IDs Here";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Navy;
-            this.ClientSize = new System.Drawing.Size(384, 374);
+            this.ClientSize = new System.Drawing.Size(634, 350);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnPrev);
             this.Controls.Add(this.labelPageNum);
             this.Controls.Add(this.txtNewsBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtGameId);
-            this.Controls.Add(this.button1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -129,14 +204,18 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtGameId;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNewsBox;
         private System.Windows.Forms.Label labelPageNum;
         private System.Windows.Forms.Button btnPrev;
         private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- testSharp/Form1.cs
+++ testSharp/Form1.cs
@@ -21,89 +21,11 @@ namespace testSharp
         public Form1()
         {
             InitializeComponent();
-            //REST to get my recently played games which I'll use to fill the combo box
-            var client = new RestClient("http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/");
-            var request = new RestRequest();
-
-            //parameter setup
-            request.AddParameter("key", "71897F1E822664717BC6FEFAE509510A");
-            request.AddParameter("steamid", "76561198165275807");
-            request.AddParameter("count", "5");
-            request.AddParameter("format", "json");
-
-            //SERvice call
-            var response = client.Execute(request);
-
-            JsonDeserializer deserializer = new JsonDeserializer();
-            MessageBox.Show(response.Content);
-            //setting up contents or response neatly
-            var temp = new rootObject2();
-            temp = deserializer.Deserialize<rootObject2>(response);
-            var games = temp.res.gamesList;
-
-            string[] allGames = new string[games.Count + 1];
-            int totalGames = games.Count;
-
-            for (int i = 0; i < games.Count; i++)
-            {
-                allGames[i] = games[i].toString();
-                MessageBox.Show(allGames[i]);
-            }
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            try {
-                //initaliazing gameId
-                int gameId = 0;
-
-                
-                //SECOND REST -- SECOND REST -- SECOND REST -- SECOND REST -- SECOND REST -- SECOND REST -- 
-
-                //getting id of game from input box
-                gameId = int.Parse(txtGameId.Text);
-
-                //client and request setup
-                var client = new RestClient("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?");
-                var request = new RestRequest();
-
-                // Set up and build parameter list for this service
-                //string key = "71897F1E822664717BC6FEFAE509510A";
-                //request.AddParameter("api key", key);
-                request.AddParameter("appid", gameId);
-                request.AddParameter("count", "10"); //max amount of news articles to bring back
-                request.AddParameter("maxlength", "600"); //max amount of characters taken from news article
-                request.AddParameter("format", "json");
-
-                // Service call
-                var response = client.Execute(request);
-
-                var deserializer = new JsonDeserializer();
-                //setting up contents or response neatly
-                var temp2 = new RootObject();
-                temp2 = deserializer.Deserialize<RootObject>(response);
-                var news = temp2.appNews.newsItems;
-
-                newsArticles = new string[news.Count + 1];
-                totalArticles = news.Count;
-                currArticle = 0;
-
-                for (int i = 0; i < news.Count; i++) {
-                    newsArticles[i] = news[i].toString();
-                }
-
-
-                txtNewsBox.Text = newsArticles[currArticle];
-                labelPageNum.Text = (currArticle + 1) + "/" + totalArticles;
-            }
-            catch (FormatException)
-            {
-                MessageBox.Show("Game ID must be number");
-            } catch (NullReferenceException nullEx)
-            {
-                MessageBox.Show("No Game with that id");
-            }
+            
         }
 
         public class appnews {
@@ -139,32 +61,7 @@ namespace testSharp
             public appnews appNews { get; set; }
         }
 
-        public class resp
-        {
-            public int total_count { get; set; }
-            public List<games> gamesList { get; set; }
-        }
-
-        public class rootObject2{
-            public resp res { get; set; }
-        }
-
-        
-
-        public class games
-        {
-            public int appid { get; set; }
-            public string name { get; set; }
-            public int playtime_2weeks { get; set; }
-            public int playtime_forever { get; set; }
-            public string img_icon_url { get; set; }
-            public string img_logo_url { get; set; }
-
-            public string toString()
-            {
-                return "name= " + name;
-            }
-        }
+       
 
 private void btnNext_Click(object sender, EventArgs e)
         {
@@ -196,6 +93,73 @@ private void btnNext_Click(object sender, EventArgs e)
                 txtNewsBox.Text = newsArticles[currArticle];
                 labelPageNum.Text = (currArticle + 1) + "/" + totalArticles;
             }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                //initaliazing gameId
+                int gameId = 0;
+
+
+                //SECOND REST -- SECOND REST -- SECOND REST -- SECOND REST -- SECOND REST -- SECOND REST -- 
+
+                //getting id of game from input box
+                gameId = int.Parse(txtGameId.Text);
+
+                //client and request setup
+                var client = new RestClient("http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?");
+                var request = new RestRequest();
+
+                // Set up and build parameter list for this service
+                //string key = "71897F1E822664717BC6FEFAE509510A";
+                //request.AddParameter("api key", key);
+                request.AddParameter("appid", gameId);
+                request.AddParameter("count", "10"); //max amount of news articles to bring back
+                request.AddParameter("maxlength", "900"); //max amount of characters taken from news article
+                request.AddParameter("format", "json");
+
+                // Service call
+                var response = client.Execute(request);
+
+                var deserializer = new JsonDeserializer();
+                //setting up contents or response neatly
+                var temp2 = new RootObject();
+                temp2 = deserializer.Deserialize<RootObject>(response);
+                var news = temp2.appNews.newsItems;
+
+                newsArticles = new string[news.Count + 1];
+                totalArticles = news.Count;
+                currArticle = 0;
+
+                for (int i = 0; i < news.Count; i++)
+                {
+                    newsArticles[i] = news[i].toString();
+                }
+
+
+                txtNewsBox.Text = newsArticles[currArticle];
+                labelPageNum.Text = (currArticle + 1) + "/" + totalArticles;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Game ID must be number");
+            }
+            catch (NullReferenceException nullEx)
+            {
+                MessageBox.Show("No Game with that id");
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
  }
